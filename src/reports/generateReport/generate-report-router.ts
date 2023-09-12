1898d1ff75d88915e1e837ab60d6a3d4a3a319b8
TheGuzman <guzmanmedrano.diego@gmail.com>
2023-09-12 17:29:51 +0200

    fix(puppeteer): remove headless mode and add padding to titles

--- src/reports/generateReport/generate-report-router.ts
+++ src/reports/generateReport/generate-report-router.ts
@@ -57,7 +57,7 @@ export default generateReportRouter;
 
 const printPDF = async (reportData: ReportView) => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     ignoreDefaultArgs: ['--disable-extensions'],
     args: [
       '--disable-setuid-sandbox',
